c06e6c5639bfcb79fd74b3bde150e632cc3fd2c2
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-02-27 15:12:18 +0530

    This pattern is same as the first one but in reverse

---
+++ Patter-Problems/3-Reverse-Triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+Python programm to make the following pattern
+
+'''
+
+num = int(input('Enter the size of the Triangle: \n'))
+
+for r in range(1,num+1):
+    for c in range(r,num+1):
+        print('*',end=' ')
+    print()
