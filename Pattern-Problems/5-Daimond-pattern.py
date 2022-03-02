1aae1cad3ecc0d0de77c42d772f63344078d412f
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-03-02 18:00:15 +0530

    Added comments about the loops to explain there use

--- Pattern-Problems/5-Daimond-pattern.py
+++ Pattern-Problems/5-Daimond-pattern.py
@@ -18,13 +18,13 @@ program to produce the following pattern programming
 
 num = int(input('Enter the size of the Diamond: '))
 
-for r in range(num+1):
+for r in range(num+1):            # this loop is same as for pyramid
     for c in range(r,num):
         print(end=" ")
     for c in range(r+1):
         print('*',end = " ")
     print()
-for r in range(num):
+for r in range(num):              # this loop prints the pyramid in reverse 
     for c in range(r+1):
         print(end=" ")
     for c in range(r,num):
