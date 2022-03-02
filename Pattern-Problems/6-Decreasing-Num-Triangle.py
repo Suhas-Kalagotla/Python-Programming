7f36315eb30717b65851c7c220c2504af23220e1
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-03-02 18:02:16 +0530

    added comments to explain how the numbers are decreased using range function

--- Pattern-Problems/6-Decreasing-Num-Triangle.py
+++ Pattern-Problems/6-Decreasing-Num-Triangle.py
@@ -3,7 +3,7 @@
 '''
 python programm to make the following pattern
 
-1 
+1
 2 1
 3 2 1
 4 3 2 1
@@ -13,6 +13,6 @@ python programm to make the following pattern
 
 num = int(input('Enter the size of the triangle: '))
 for r in range(1,num+1):
-    for c in range(r,0,-1):
+    for c in range(r,0,-1):  # this range function helps to decrease the numbers by -1 step
         print(c,end=' ')
     print()
