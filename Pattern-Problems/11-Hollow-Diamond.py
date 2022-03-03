c918764f99e566696d429e13fc0b581b59a332f2
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-03-03 22:10:43 +0530

    An interesting pattern but made using previous pattern programs

---
+++ Pattern-Problems/11-Hollow-Diamond.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+'''
+Python programme to generate the following pattern
+'''
+
+num = int(input('Enter the size of the diamond: '))
+
+for r in range(num):
+    for c in range(r,num-1):
+        print(end=' ')     # single spacing
+    for c in range(r+1):
+        if c== 0 or c ==r :
+            print('*',end = ' ')
+        else:
+            print(end='  ')  # double spacing
+    print()
+for x in range(num):
+    for y in range(x):
+        print(end=' ') # single spacing
+    for y in range(x,num):
+        if y == 0 or y == x or y == num-1 :
+            print('*',end=' ')
+        else:
+            print(end='  ') # double spacing
+    print()
