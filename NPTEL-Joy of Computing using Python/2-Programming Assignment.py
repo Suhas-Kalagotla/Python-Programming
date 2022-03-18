7cc598ecf10b8362cb366ae5495bf26c993990ca
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-03-18 11:38:27 +0530

    Week-2

---
+++ NPTEL-Joy of Computing using Python/2-Programming Assignment.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+'''
+Take two integers as an input and display the difference of second number from first number.
+
+Input
+x
+y
+
+Output
+x-y
+'''
+
+num1 = int(input())
+num2 = int(input())
+
+diff = num1-num2
+
+print(diff)
