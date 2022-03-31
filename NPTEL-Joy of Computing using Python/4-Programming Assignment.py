31af7c274d164afc418c6a10dd0647cc9aa657cd
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-03-31 15:15:01 +0530

    updated the questions

--- NPTEL-Joy of Computing using Python/4-Programming Assignment.py
+++ NPTEL-Joy of Computing using Python/4-Programming Assignment.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 '''
-You are given a list named L. Print all the elements at odd position of list L.(We will take care of the input, you just have to print elements present at odd position)
+You are given a list named L. Print all the elements at odd position of list L.
+(We will take care of the input, you just have to print elements present at odd position)
 
 Input:
 
--- NPTEL-Joy of Computing using Python/6-Programming Assignment.py
+++ NPTEL-Joy of Computing using Python/6-Programming Assignment.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 '''
-You are given a list L. Write a function all_even that accepts the list L and print all the even numbers is the list L.(Order of the numbers should be same as the order present in the list)
+You are given a list L. Write a function all_even that accepts the list L
+and print all the even numbers is the list L.
+(Order of the numbers should be same as the order present in the list)
 
 We will take care of the input.
 
@@ -22,4 +24,4 @@ def all_even(L) :
         if num % 2 == 0 :
             print(num)
 
-all_even(L) # given line of code 
+all_even(L) # given line of code
