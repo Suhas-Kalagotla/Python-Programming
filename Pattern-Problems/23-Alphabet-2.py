91f142d792767d103afa1af7b755115ef8c97259
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-03-31 15:22:13 +0530

    Every character changing

---
+++ Pattern-Problems/23-Alphabet-2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+'''
+programme to produce following type of pattern
+
+sammple output:
+A B C D E
+F G H I J
+K L M N O
+P Q R S T
+U V W X Y
+'''
+num = 0
+for r in range(5):
+    for c in range(5):
+        print(chr(65+num),end=' ')
+        num += 1
+    print()
