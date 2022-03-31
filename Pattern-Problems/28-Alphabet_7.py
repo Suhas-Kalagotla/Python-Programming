9c63e0dda8d99dc058814c5bceef7a3bdae52c37
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-03-31 23:06:34 +0530

    28-Alphabet_7.py

---
+++ Pattern-Problems/28-Alphabet_7.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+'''
+python programme to make the following pattern
+
+sample input:
+Enter the size of the triangle: 6
+
+sample output:
+A
+AB
+A B
+A  B
+A   B
+ABCDEF
+'''
+size = int(input('Enter the size of the triangle: '))
+
+for r in range(1,size+1):
+    count = 0
+    for c in range(r):
+        if c == 0 or c == r-1 or r == size : #print only in first or last column or last row
+            print(chr( 65 + count ) , end='')
+            count += 1
+        else:
+            print(end=' ')
+    print()
