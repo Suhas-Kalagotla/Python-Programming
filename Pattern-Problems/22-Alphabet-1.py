de6a2f8d3a2856394c917944e4ab9a4b8fe3a323
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-03-31 14:57:18 +0530

    first alphabet pattern

---
+++ Pattern-Problems/22-Alphabet-1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+here we are looping through alphabets and printing them
+we are using a function called chr
+
+output:
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+'''
+for i in range(65,91):
+    print(chr(i),end=' ')
